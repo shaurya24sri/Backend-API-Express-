1cadeeb746086492e484bc5d0e634366ddd123f4
Shaurya Srivastava <81847195+shaurya24sri@users.noreply.github.com>
2024-06-08 22:34:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
+import { name } from "ejs";
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017", {
+    dbname: "backend",
+  })
+  .then(() => console.log("Database connected"))
+  .catch(() => console.log(e));
+
+//schema
+const msgschema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+//model==collection(messages)
+const message = new mongoose.model("Message", msgschema);
 
 const app = express();
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: "true" }));
 
 app.set("view engine", "ejs");
-const users = [];
+
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -17,11 +34,13 @@ app.get("/success", (req, res) => {
 app.get("/users", (req, res) => {
   res.json({ users });
 });
-app.post("/", (req, res) => {
-  users.push({ username: req.body.name, email: req.body.email });
+app.post("/", async (req, res) => {
+  const { name, email } = req.body;
+  await message.create({ name, email });
   res.redirect("/success");
 });
 
+//server
 app.listen(5000, () => {
   console.log("Server is running...!");
 });
